474cf813166f6b4ed4d80da807fac7c2390a261e
Gokce <Ulusoygokce88@gmail.com>
2025-02-03 13:19:01 +0300

    vs code kullanılarak git depomuzu oluşturduk

---
+++ #Front seat conditions.py
@@ -0,0 +1,20 @@
+#Front seat conditions 
+#Bu kodumuz ile belli yaş altındaki bireylerin ön koltukta oturma koşunu inceliyoruz.
+passenger_age = int(input("Enter your age please"))
+if passenger_age < 12:
+    print("You can't front passenger seat")
+else:
+    print("you can front passenger seat") 
+
+
+
+
+
+
+
+
+
+
+
+
+
