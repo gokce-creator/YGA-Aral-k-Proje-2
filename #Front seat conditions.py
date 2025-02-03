bf68e5ded4c45a84f745769707435e31348a617d
Gokce <Ulusoygokce88@gmail.com>
2025-02-03 13:46:08 +0300

    Rapor dosyası oluşturuldu ve rapor yazıldı.

--- #Front seat conditions.py
+++ #Front seat conditions.py
@@ -1,6 +1,9 @@
 #Front seat conditions 
 #Bu kodumuz ile belli yaş altındaki bireylerin ön koltukta oturma koşunu inceliyoruz.
-passenger_age = int(input("Enter your age please"))
+try:
+    passenger_age = int(input("Enter your age please: "))
+except ValueError:
+    print("Invalid input! Please enter a number.")
 if passenger_age < 12:
     print("You can't front passenger seat")
 else:
